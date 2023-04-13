cdd486b059edc8829c295b04733e55504f6b0909
Alek30k <alemank23@gmail.com>
2023-04-13 09:36:16 -0400

    last commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,4 @@
 import Head from "next/head";
-// import Image from "next/image";
-// import Featured from "../components/Featured";
-// import PizzaList from "../components/PizzaList";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
@@ -11,9 +8,8 @@ export default function Home() {
         <title>Pizza Restaurant in Newyork</title>
         <meta name="description" content="Best pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
+        Homepage
       </Head>
-      {/* <Featured/> */}
-      {/* <PizzaList /> */}
     </div>
   );
 }
