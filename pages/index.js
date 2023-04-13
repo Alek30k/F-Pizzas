e3cfaf9563e5e2433963406f3e1cdf18a1f13a76
Alek30k <alemank23@gmail.com>
2023-04-13 10:01:23 -0400

    last commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,15 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <div className={styles.container}>
-      <Navbar />
       <Head>
         <title>Pizza Restaurant in Newyork</title>
         <meta name="description" content="Best pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
-        Homepage
       </Head>
+      Homepage
     </div>
   );
 }
