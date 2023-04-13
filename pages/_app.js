a8944801afafc8c9e53cb7ba457b3680483b6a13
Alek30k <alemank23@gmail.com>
2023-04-13 09:23:10 -0400

    last commit

---
+++ pages/_app.js
@@ -0,0 +1,13 @@
+// import Layout from "../components/Layout";
+import "../styles/globals.css";
+
+function MyApp() {
+  return (
+    <div>
+      {/* <Component {...pageProps} /> */}
+      Hola
+    </div>
+  );
+}
+
+export default MyApp;
