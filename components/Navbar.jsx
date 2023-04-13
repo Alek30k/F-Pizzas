69fe9335b7d35fa8fdbcf3397b3e63dbf3c9ed69
Alek30k <alemank23@gmail.com>
2023-04-13 14:39:20 -0400

    last commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,18 @@
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.callButton}>
+        <Image src="/img/telephone.png" alt="" width="32" height="32" />
+      </div>
+      <div className={styles.texts}>
+        <div className={styles.text}>ORDER NOW!</div>
+        <div className={styles.text}>012 345 678</div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
