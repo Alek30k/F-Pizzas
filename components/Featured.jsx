00210b16e9b125bda38fe28796d14834336213d6
Alek30k <alemank23@gmail.com>
2023-04-15 14:30:23 -0400

    last commit

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -7,6 +7,13 @@ const Featured = () => {
       <div className={styles.arrowContainer}>
         <Image src="/img/arrowl.png" alt="" width="100" height="150" />
       </div>
+      <div
+        className={styles.arrowContainer}
+        style={{ right: 0 }}
+        onClick={() => handleArrow("r")}
+      >
+        <Image src="/img/arrowr.png" layout="fill" alt="" objectFit="contain" />
+      </div>
     </div>
   );
 };
