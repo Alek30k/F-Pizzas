12c61bc704aa105796eb06ebd342fc121e3d8ccd
Alek30k <alemank23@gmail.com>
2023-04-15 14:21:55 -0400

    last commit

--- components/Featured.jsx
+++ components/Featured.jsx
@@ -1,5 +1,18 @@
+import styles from "../styles/Featured.module.css";
+import Image from "next/image";
+
 const Featured = () => {
-  return <div>Featured</div>;
+  return (
+    <div style={styles.container}>
+      <div
+        className={styles.arrowContainer}
+        style={{ left: 0 }}
+        onClick={() => handleArrow("l")}
+      >
+        <Image src="/img/arrowl.png" alt="" layout="fill" objectFit="contain" />
+      </div>
+    </div>
+  );
 };
 
 export default Featured;
