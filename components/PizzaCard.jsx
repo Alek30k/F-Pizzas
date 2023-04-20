a5bd0aac9e824a9d6efe5bd04e487c5d161ba8a3
Alek30k <alemank23k@gmail.com>
2023-04-20 16:08:19 -0300

    last commit

--- components/PizzaCard.jsx
+++ components/PizzaCard.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import styles from "../styles/PizzaCard.module.css";
 
-const PizzaCard = () => {
+const PizzaCard = ({ pizza }) => {
   return (
     <div className={styles.container}>
       <Image src="/img/pizza.png" alt="" width="500" height="500" />
