feb265844254e5fd9fa9932e4c35f19b8d6c81ab
Alek30k <alemank23k@gmail.com>
2023-04-20 15:58:43 -0300

    last commit

--- pages/index.js
+++ pages/index.js
@@ -16,3 +16,20 @@ export default function Home() {
     </div>
   );
 }
+
+export const getServerSideProps = async (ctx) => {
+  const myCookie = ctx.req?.cookies || "";
+  let admin = false;
+
+  if (myCookie.token === process.env.TOKEN) {
+    admin = true;
+  }
+
+  const res = await axios.get("http://localhost:3000/api/products");
+  return {
+    props: {
+      pizzaList: res.data,
+      admin,
+    },
+  };
+};
