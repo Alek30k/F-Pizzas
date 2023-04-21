91e78a66799ffe558af319e3dffe508b01832925
Alek30k <alemank23k@gmail.com>
2023-04-21 16:15:53 -0300

    last commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { Provider } from "react-redux";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
+import store from "../redux/store";
 
 function MyApp({ Component, pageProps }) {
   return (
