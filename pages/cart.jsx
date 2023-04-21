5aed55608ab83e2701dd0651cb2099ef30546d88
Alek30k <alemank23k@gmail.com>
2023-04-21 17:09:35 -0300

    last commit

--- pages/cart.jsx
+++ pages/cart.jsx
@@ -1,5 +1,7 @@
 import styles from "../styles/Cart.module.css";
 import Image from "next/image";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 const Cart = () => {
   return (
